fc425909e992384e706a6abd7eefe8fc6ee0db3f
Guilherme Camboim <105326604+guilhermecamboim@users.noreply.github.com>
2023-03-05 18:36:44 -0300

    :sparkles: feat(Itens): Creating itens section.

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categoriesSlice from "./reducers/categorias";
 import itensSlice from "./reducers/itens";
+import carrinhoSlice from "./reducers/carrinho";
 
 const store = configureStore({
   reducer: {
     categorias: categoriesSlice,
     itens: itensSlice,
+    carrinho: carrinhoSlice,
   },
 });
 
