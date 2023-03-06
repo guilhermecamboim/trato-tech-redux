8d4ac1d89d2deeeefd540692cfa0d7640fdd1ac1
Guilherme Camboim <105326604+guilhermecamboim@users.noreply.github.com>
2023-03-05 23:26:21 -0300

    :sparkles: feat(Project): Finishing the project.

--- src/components/Busca/index.js
+++ src/components/Busca/index.js
@@ -1,12 +1,25 @@
-import styles from './Busca.module.scss';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+import { mudarBusca, resetarBusca } from "store/reducers/busca";
+import styles from "./Busca.module.scss";
 
 export default function Busca() {
+  const busca = useSelector((state) => state.busca);
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  useEffect(() => {
+    dispatch(resetarBusca());
+  }, [location.pathname, dispatch]);
   return (
     <div className={styles.busca}>
       <input
         className={styles.input}
         placeholder="O que você procura?"
+        onChange={(e) => dispatch(mudarBusca(e.target.value))}
+        value={busca}
       />
     </div>
-  )
+  );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import categoriesSlice from "./reducers/categorias";
 import itensSlice from "./reducers/itens";
 import carrinhoSlice from "./reducers/carrinho";
+import buscaSlice from "./reducers/busca";
 
 const store = configureStore({
   reducer: {
     categorias: categoriesSlice,
     itens: itensSlice,
     carrinho: carrinhoSlice,
+    busca: buscaSlice,
   },
 });
 
